be700acf1daff013eb79ef26ab5c2ac44c139268
Chinedu Ike <chineduike@Chinedus-MacBook-Pro.local>
2025-12-21 12:36:47 +0000

    new

--- Jenkins_server/main.tf
+++ Jenkins_server/main.tf
@@ -59,6 +59,7 @@ sudo systemctl status jenkins
 sudo yum install -y git
 sudo yum install -y docker
 sudo service docker start
+sudo usermod -aG docker ec2-user
 sudo usermod -aG docker jenkins
 sudo systemctl restart jenkins
 EOF
